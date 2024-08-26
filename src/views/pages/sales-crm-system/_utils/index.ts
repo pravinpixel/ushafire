aff0d69b9b8860704156b225f4a63f76669a2145
ChairmaDuraiCape <127489359+ChairmaDuraiCape@users.noreply.github.com>
2024-08-26 10:20:51 +0530

    Revert "initial commit"
    This reverts commit 9f27e877c9ba52febc9a7a4079009a2fa0e04175.

--- src/views/pages/sales-crm-system/_utils/index.ts
+++
@@ -1,7 +0,0 @@
-import { checkStock } from './CheckStock';
-import LoanGroupTable from './LoanGroupTable';
-import CrmProductGroup from './CrmProductGroup';
-import SaleOrderGroupTable from './SaleOrderGroupTable';
-import BlanketOrderGroupTable from './BlanketOrderGroupTable';
-
-export { checkStock, LoanGroupTable, CrmProductGroup, SaleOrderGroupTable, BlanketOrderGroupTable };
