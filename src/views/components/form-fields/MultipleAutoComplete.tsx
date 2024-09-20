a1379f74455fe06ee796ce0e5ef67c21a5bcaaa7
Sowmiya <sowmiya@capecomsolutions.com>
2024-09-20 18:50:28 +0530

    multiselect issue fixed

--- src/views/components/form-fields/MultipleAutoComplete.tsx
+++ src/views/components/form-fields/MultipleAutoComplete.tsx
@@ -1,8 +1,7 @@
-import { Autocomplete, Box, Button, FormLabel, TextField} from "@mui/material";
+import { Autocomplete, Box, Button, FormLabel, MenuItem, TextField } from "@mui/material";
 import { useController, useFormContext } from "react-hook-form";
 import { DownArrowTask } from "../../../utils/theme/svg";
 import { useState } from "react";
-import { ListSubheader } from "@mui/material";
 
 const MultiAutoCompleteField = (props: {
     label: string;
@@ -10,7 +9,7 @@ const MultiAutoCompleteField = (props: {
     options?: EssentailTypeListResponse[];
     disabledArray?: string[]
 }) => {
-    const { label, name, options = [] , disabledArray = []} = props;
+    const { label, name, options = [], disabledArray = [] } = props;
     const { control } = useFormContext();
     const {
         field,
@@ -24,12 +23,15 @@ const MultiAutoCompleteField = (props: {
     const [open, setOpen] = useState(false);
     const handleOpen = () => {
         setOpen(true);
-      };
-      const handleClose = () => {
+    };
+    const handleClose = () => {
         setOpen(false);
-      };
+    };
+    const handleOpenCloseToggle = () => {
+        setOpen(prev => !prev);
+    };
     const errorMessage = error?.message;
-    const handleSelectionChange = (_ : unknown, newValue : EssentailTypeListResponse[]) => {
+    const handleSelectionChange = (_: unknown, newValue: EssentailTypeListResponse[]) => {
         const selectedIds = newValue.map(option => option.id).join(",");
         field.onChange(selectedIds);
     };
@@ -38,91 +40,126 @@ const MultiAutoCompleteField = (props: {
 
     return (
         <>
-          	<Box sx={{
-			'& .MuiSelect-select': {
-				padding: "7px 14px !important"
-			},
-            '& .MuiIconButton-root':{
-                background:'none !important',
-                color:'rgba(5, 5, 5, 1)'
-            },
-		}}>
-			<FormLabel>{label}</FormLabel>
-			<Autocomplete
-             disableCloseOnSelect
-             multiple
-             open={open}
-             onOpen={handleOpen}
-             onClose={(_, reason) => {
-                if (reason === 'escape') handleClose();
-              }}
-                value={dropDowns.filter(option => value.includes(String(option.id)))}
-                onChange={handleSelectionChange}
-				onBlur={field.onBlur}
-				options={dropDowns}
-				getOptionLabel={(option) => option.name || ""}
-                getOptionDisabled={(option) => disabledArray.includes(String(option.id))}
-                renderTags={(selected) =>
-                    selected.map((option, index) => (
-                      
-                        <span key={option.id} >
-                            <Box sx={{paddingLeft:'8px',display:'flex',flexWrap:'wrap',flexDirection:'column'}}>
-                            {index > 0 && ", "}
-                            {option.name}
-                            </Box>
-                        </span>
-                        
-                    ))
-                }
-				renderInput={(params) => (
-					<TextField
-						{...params}
-						error={!!errorMessage}
-						helperText={errorMessage}
-						variant="outlined"
-						size="small"
-                        placeholder="Enter task followers name"
-						
-					/>
-				)}
-                renderOption={(props, option) => (
-                    <li {...props}>
-                        {option.name}
-                    </li>
-                )}
-                ListboxComponent={(listboxProps) => (
-                    <Box>
-                        <ul {...listboxProps} />
-                        <ListSubheader
-                            sx={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                borderTop: '1px solid #ddd',
-                                paddingTop: "18px",
-                            }}
-                        >
-                            <Button sx={{
-                            width: 120,
-                            padding: 1,
-                        }} size="small" onClick={handleClose}>
-                                Close
-                            </Button>
-                        </ListSubheader>
-                    </Box>
-                )}
-				size="small"
-				sx={{ width: '100%', mt: 1 ,"& .MuiAutocomplete-root":{
-                    paddingLeft:'10px'
-                }}}
-                popupIcon={
-                    <DownArrowTask sx={{width:19,margin:0.2}}/>
+            <Box sx={{
+                '& .MuiSelect-select': {
+                    padding: "7px 14px !important"
+                },
+                '& .MuiIconButton-root': {
+                    background: 'none !important',
+                    color: 'rgba(5, 5, 5, 1)'
+                },
+            }}>
+                <FormLabel>{label}</FormLabel>
+                <Autocomplete
+                    disableCloseOnSelect
+                    multiple
+                    open={open}
+                    onOpen={handleOpen}
+                    onClose={handleClose}
+                    value={dropDowns.filter(option => value.includes(String(option.id)))}
+                    onChange={handleSelectionChange}
+                    // onBlur={field.onBlur}
+                    options={dropDowns}
+                    getOptionLabel={(option) => option.name || ""}
+                    getOptionDisabled={(option) => disabledArray.includes(String(option.id))}
+                    renderTags={(selected) =>
+                        selected.map((option, index) => (
 
-                }
-                
-			/>
-            
-		</Box>
+                            <span key={option.id} >
+                                <Box sx={{ paddingLeft: '8px', display: 'flex', flexWrap: 'wrap', flexDirection: 'column' }}>
+                                    {index > 0 && ", "}
+                                    {option.name}
+                                </Box>
+                            </span>
+
+                        ))
+                    }
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            error={!!errorMessage}
+                            helperText={errorMessage}
+                            variant="outlined"
+                            size="small"
+                            placeholder="Enter task followers name"
+
+                        />
+                    )}
+                    renderOption={(props, option) => (
+                        <>
+                            <MenuItem  {...props} component="li"
+
+                            >
+                                {option.name}
+                            </MenuItem>
+                            {option.id === dropDowns[dropDowns.length - 1].id && (
+                                <Box sx={{
+                                    display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 7, backgroundColor: 'white', position: 'fixed',
+                                    bottom: 0, width: '100%'
+                                }}>
+                                    <Button sx={{
+                                        width: 120,
+                                        padding: 1,
+                                        marginY: { xs: 2, sm: 1 },
+
+                                    }} size="small" onClick={handleClose}>
+                                        Close
+                                    </Button>
+                                </Box>
+                            )}
+                        </>
+                    )}
+                    // ListboxComponent={(listboxProps) => (
+                    //     <>
+                    //     {
+                    //         console.log(listboxProps,"listbox")
+                    //     }
+                    //      <Box>
+
+
+
+                    //        <ul {...listboxProps} 
+
+                    //        />
+                    //        <ListSubheader
+                    //            sx={{
+                    //                display: 'flex',
+                    //                alignItems: 'center',
+                    //                justifyContent: 'center',
+                    //                borderTop: '1px solid #ddd',
+                    //                paddingTop: "18px",
+                    //            }}
+                    //        >
+                    //            <Button sx={{
+                    //            width: 120,
+                    //            padding: 1,
+                    //        }} size="small" onClick={handleClose}>
+                    //                Close
+                    //            </Button>
+                    //        </ListSubheader>
+                    //    </Box>
+                    //     </>
+
+                    // )}
+                    size="small"
+                    sx={{
+                        width: '100%', mt: 1, "& .MuiAutocomplete-root": {
+                            paddingLeft: '10px'
+                        }
+                    }}
+                    popupIcon={
+                        // <DownArrowTask sx={{width:19,margin:0.2}}  onClick={handleOpenCloseToggle}/>
+                        <Box
+                            sx={{ display: 'flex', alignItems: 'center' }}
+                            onClick={handleOpenCloseToggle}
+                        >
+                            <DownArrowTask sx={{ width: 20, margin: 0.2 }} />
+                        </Box>
+                    }
+
+                />
+
+            </Box>
         </>
     );
 };
