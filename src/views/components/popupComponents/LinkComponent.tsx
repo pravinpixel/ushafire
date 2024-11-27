5ec23f9c242519503d36becee569767c88b5ed98
abisharajathurai <144227977+abisharajathurai@users.noreply.github.com>
2024-11-27 17:36:21 +0530

    commit for linkify text issue

--- src/views/components/popupComponents/LinkComponent.tsx
+++ src/views/components/popupComponents/LinkComponent.tsx
@@ -2,12 +2,23 @@ import { Box } from "@mui/material"
 import Linkify from "linkify-react"
 
 const LinkComponent = ({ text = '' }) => {
+    const handleLinkClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        const target = event.target as HTMLAnchorElement;
+        if (target.tagName.toLowerCase() === 'a' && target.href) {
+            event.preventDefault();
+            event.stopPropagation();
+            window.open(target.href, '_blank', 'noopener,noreferrer');
+        }
+    }
     return (
-        <Box className="link_anchor_tag" sx={{wordBreak:'break-all'}} onClick={(e) => {
-            e.stopPropagation()
-        }}>
+        <Box className="link_anchor_tag" sx={{wordBreak:'break-all'}} 
+        onClick={handleLinkClick}
+        >
             <Linkify options={{
                 target: "_blank",
+                attributes:{
+                    onClick: (event: React.MouseEvent) => event.stopPropagation()
+                }
             }}>
                 {String(text)}
             </Linkify>
